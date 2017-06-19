54155c5f04811dff665bae035c33f161bae0064c
wrathematics <wrathematics@gmail.com>
2017-06-19 13:20:49 -0400

    add benchmark example

---
+++ examples/benchmark.cpp
@@ -0,0 +1,25 @@
+#include "../src/rsvd.h"
+
+#include <cstdio>
+#include <ctime>
+
+
+int main()
+{
+  const int m = 10000;
+  const int n = 250;
+  const int k = 3;
+  
+  arma::mat x(m, n);
+  x.randn();
+  
+  RSVD rsvd(1234);
+  clock_t start = clock();
+  rsvd.rsvd(k, true, true, x);
+  clock_t end = clock();
+  double rsvd_time = (double) (end - start) / CLOCKS_PER_SEC;
+  
+  printf("Run time: %f\n", rsvd_time);
+  
+  return 0;
+}
